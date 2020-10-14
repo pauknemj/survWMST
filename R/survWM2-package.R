d609699f7b40e8c6d08214b559c57e7802b48769
pauknemj <57009714+pauknemj@users.noreply.github.com>
2020-10-14 17:06:56 -0500

    Add files via upload

---
+++ R/survWM2-package.R
@@ -0,0 +1,22 @@
+#' @name survWM2-package
+#' @aliases  survWM2-package
+#' @docType  package
+#' @title Comparing Window Mean Survival Time
+#' @description
+#' Performs two-sample comparisons using the restricted mean survival time (RMST) as a summary measure of the survival time distribution.
+#' Three kinds of between-group contrast metrics (i.e., the difference in WMST, the ratio of WMST and the ratio of the window mean time lost (WMTL)) are computed.
+#' @author Mitchell Paukner
+#'
+#' Maintainer: Mitchell Paukner <mpaukner@wisc.edu>
+#' @references
+#' Figure out references (might need to cite yourself)
+#' @keywords
+#' survival
+#' @seealso
+#' survival
+#' @import survival
+#' @import hesim
+#' @importFrom stats glm lm pchisq pnorm qnorm
+#' @importFrom utils data
+
+NULL
