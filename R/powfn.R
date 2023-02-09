ec965ee11182da41e67ef70651f495cdc8535337
pauknemj <57009714+pauknemj@users.noreply.github.com>
2023-02-09 11:56:56 -0600

    Add files via upload

--- R/powfn.R
+++ R/powfn.R
@@ -2,10 +2,10 @@
 # powfn (two-arm) -- hidden
 ################################
 
-powfn<-function(survC, survT, eperiod, end_study, tau0, tau1, n, alpha){
+powfn<-function(survC, survT, eperiod, end_study, tau0, tau1, n0, n1, alpha){
   WMST_truediff<-integrate(function(x) survT$S(x)-survC$S(x),
                            lower = tau0, upper = tau1)$value
-  WMST_trueSE<-sqrt(evar(survT, eperiod, end_study, tau0, tau1, n/2)+
-                      evar(survC, eperiod, end_study, tau0, tau1, n/2))
+  WMST_trueSE<-sqrt(evar(survT, eperiod, end_study, tau0, tau1, n0)+
+                      evar(survC, eperiod, end_study, tau0, tau1, n1))
   return(1-pnorm(qnorm(1-alpha)-WMST_truediff/WMST_trueSE))
 }
